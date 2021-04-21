7358ec3adda7e444da4fce11c5d31d739c4cfc89
WJC <wj.colley@hotmail.co.uk>
2021-04-21 15:32:25 +0100

    Returns table with 1 hardcoded bus stop

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
 
+atco="490008660N"
+
 @app.route('/')
-def hello_world(name="Test Input"):
-    return render_template('index.html', name=name)
+def hello_world():
+    params = {
+        'app_id': '41cd49ac',
+        'app_key': '8c415e3902409ebb511bffaa9e1b1dcc',
+        'group': 'no'
+    }
+    
+    results = requests.get('http://transportapi.com/v3/uk/bus/stop/' + atco + '/live.json', params=params).json()
+    buses = results['departures']['all']
+
+    return render_template('index.html', buses=buses)
+
+
+@app.route('/nextBuses')
+def next_buses():
+    params = {
+        'app_id': '41cd49ac',
+        'app_key': '8c415e3902409ebb511bffaa9e1b1dcc',
+        'group': 'no'
+    }
+    
+    results = requests.get('http://transportapi.com/v3/uk/bus/stop/' + atco + '/live.json', params=params).json()
+    buses = results['departures']['all']
+
+    return buses
